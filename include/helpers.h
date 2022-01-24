8d0d54d27c6a33f7b69b3ef2261724671d621946
Nis Meinert <nis.meinert@gmail.com>
2022-01-24 20:41:10 +0100

    Add documentation to helpers.h

--- include/helpers.h
+++ include/helpers.h
@@ -1,6 +1,20 @@
+/*!
+ * \file helpers.h
+ * \brief Compilation of helper functions.
+ */
+
 #ifndef LIBGRAVIX2_HELPERS_H
 #define LIBGRAVIX2_HELPERS_H
 
+/*!
+ * \brief Computes \f$\sin(x) / x\f$.
+ *
+ * Save calculation of \f$\sin(x) / x\f$. In particular, this function returns
+ * one for \f$x=0\f$.
+ *
+ * @param x \f$x\f$.
+ * @return \f$\sin(x) / x\f$.
+ */
 double sinc(double);
 
 #endif // LIBGRAVIX2_HELPERS_H
