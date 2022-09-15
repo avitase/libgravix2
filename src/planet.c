62b65244b8614c1d62f80477ce354dfd172a9a7d
Nis Meinert <mail@nismeinert.de>
2022-09-15 14:45:04 +0200

    Add grvx_get_planet()

--- src/planet.c
+++ src/planet.c
@@ -27,21 +27,38 @@ unsigned grvx_count_planets(GrvxPlanetsHandle p)
 
 int grvx_set_planet(GrvxPlanetsHandle p, unsigned i, double lat, double lon)
 {
-    if (i < p->n) {
-        const double sin_lat = sin(lat);
-        const double cos_lat = cos(lat);
-        const double sin_lon = sin(lon);
-        const double cos_lon = cos(lon);
-
-        ptrdiff_t offset = 3 * (ptrdiff_t)i;
-        p->data[offset] = cos_lat * sin_lon;
-        p->data[offset + 1] = cos_lat * cos_lon;
-        p->data[offset + 2] = sin_lat;
-
-        return 0;
+    if (i >= p->n) {
+        return -1;
     }
 
-    return -1;
+    const double sin_lat = sin(lat);
+    const double cos_lat = cos(lat);
+    const double sin_lon = sin(lon);
+    const double cos_lon = cos(lon);
+
+    ptrdiff_t offset = 3 * (ptrdiff_t)i;
+    p->data[offset] = cos_lat * sin_lon;
+    p->data[offset + 1] = cos_lat * cos_lon;
+    p->data[offset + 2] = sin_lat;
+
+    return 0;
+}
+
+int grvx_get_planet(GrvxPlanetsHandle p, unsigned i, double *lat, double *lon)
+{
+    if (i >= p->n) {
+        return -1;
+    }
+
+    ptrdiff_t offset = 3 * (ptrdiff_t)i;
+    const double x = p->data[offset];
+    const double y = p->data[offset + 1];
+    const double z = p->data[offset + 2];
+
+    *lat = asin(z);
+    *lon = atan2(x, y);
+
+    return 0;
 }
 
 unsigned grvx_pop_planet(GrvxPlanetsHandle p)
